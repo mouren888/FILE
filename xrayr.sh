8a61b7345b8387fbd3cd9daa0138b5d0387d8a02
mouren888 <85207481+mouren888@users.noreply.github.com>
2022-09-01 10:40:31 +0800

    Create xrayr.sh

---
+++ xrayr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#开启BBR
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+#下载证书到/etc
+wget --no-check-certificate https://raw.githubusercontent.com/GouGoGoal/backend/xrayr/cert.pem -O /etc/cert.pem
+wget --no-check-certificate https://raw.githubusercontent.com/GouGoGoal/backend/xrayr/key.pem -O /etc/key.pem
+#安装XrayR
+bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+#下载配置文件config.yml到/etc/XrayR/config.yml
+wget --no-check-certificate https://raw.githubusercontent.com/mouren888/FILE/main/config.yml -O /etc/XrayR/config.yml
