3431a76c45689ef9db80f80e48305555d5ea06f8
MouRen <85207481+mouren888@users.noreply.github.com>
2023-04-01 14:59:13 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -365,8 +365,8 @@ View_User_info(){
 	echo -e " 用户总流量 : ${Green_font_prefix}${transfer_enable} ${Font_color_suffix}"
 	echo -e "${ss_link}"
 	echo -e "${ssr_link}"
+	echo -e "$SSRQRcode" | qrencode -o - -t UTF8
 	echo -e " ${Green_font_prefix} 提示: ${Font_color_suffix}
-	echo -e $SSRQRcode | qrencode -o - -t UTF8
 	
  在浏览器中，打开二维码链接，就可以看到二维码图片。
  协议和混淆后面的[ _compatible ]，指的是 兼容原版协议/混淆。"
